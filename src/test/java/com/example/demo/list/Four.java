491fdd22dca220fd00f76d87651aada883f27347
caihuoche <yhy8191@163.com>
2019-08-20 18:57:09 +0800

    update:4

--- src/test/java/com/example/demo/list/Four.java
+++ src/test/java/com/example/demo/list/Four.java
@@ -11,13 +11,26 @@ public class Four {
 	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 		int mid = (nums1.length + nums2.length) / 2;
 		boolean flag = (nums1.length + nums2.length) % 2 == 1 ? true : false;
+		int[] big = nums1.length > nums2.length ? nums1 : nums2;
+		int[] small = nums1.length > nums2.length ? nums2 : nums1;
+		int left1 = 0;
 		for (int i = 0; i < mid; i++) {
-			if (nums2[mid - i - 2] < nums1[i + 1] && nums1[i] < nums2[mid - i - 1]) {
-				return i;
+			if (small[mid - i - 2] < big[i + 1] && big[i] < small[mid - i - 1]) {
+				left1 = i;
+				break;
 			}
 		}
-		System.out.println(i);
-		return 0;
+
+		if (flag) {
+			return big[left1];
+		} else {
+			int left2 = (mid - 2 - left1);
+			int left = big[left1] > small[left2] ? big[left1] : small[left2];
+			int right = big[left1 + 1] > small[left2 + 1] ? small[left2 + 1] : big[left1 + 1];
+			return (double) (left + right) / 2;
+		}
+
+
 	}
 
 	public int binarySearch(int min, int max, int[] arr, int mid) {
@@ -40,8 +53,8 @@ public class Four {
 
 	@Test
 	public void t1() {
-		int[] arr = {4, 10, 12, 24, 56};
-		int[] arr2 = {16, 17, 18, 24, 38};
+		int[] arr = {1,2};
+		int[] arr2 = {3,4};
 		double medianSortedArrays = findMedianSortedArrays(arr, arr2);
 		System.out.println(medianSortedArrays);
 		//int medianSortedArrays = binarySearch(0, arr2.length, arr2, 13);

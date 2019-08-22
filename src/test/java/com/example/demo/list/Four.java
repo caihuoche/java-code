e1295ab15aa47ab89527d0985916602200e83bfa
caihuoche <yhy8191@163.com>
2019-08-22 15:59:04 +0800

    fix:4 已经无力优化了,多次尝试才写的出来,脑回路跟不上节奏

--- src/test/java/com/example/demo/list/Four.java
+++ src/test/java/com/example/demo/list/Four.java
@@ -14,47 +14,76 @@ public class Four {
 		int[] big = nums1.length > nums2.length ? nums1 : nums2;
 		int[] small = nums1.length > nums2.length ? nums2 : nums1;
 		int left1 = 0;
-		for (int i = 0; i < mid; i++) {
-			if (small[mid - i - 2] < big[i + 1] && big[i] < small[mid - i - 1]) {
+		if (nums1.length == 0) {
+			if (!flag) {
+				return (double) (nums2[mid] + nums2[mid - 1]) / 2;
+
+			}
+			return nums2[mid];
+		}
+		if (nums2.length == 0) {
+			if (!flag) {
+				return (double) (nums1[mid] + nums1[mid - 1]) / 2;
+
+			}
+			return nums1[mid];
+		}
+		for (int i = mid - small.length - 1; i <= mid; i++) {
+			if (mid - i - 1 >= small.length) {
+				if (small[mid - (i + 1) - 1] < big[i + 1]) {
+					left1 = i;
+
+					break;
+				}
+			} else if (mid - (i + 1) - 1 == -1) {
 				left1 = i;
+				if (flag) {
+					return small[mid - i - 1] < big[i + 1] ? small[mid - i - 1] : big[i + 1];
+				}
 				break;
+			} else {
+				if (small[mid - (i + 1) - 1] <= big[i + 1] && big[i] <= small[mid - i - 1]) {
+					left1 = i;
+					if (flag) {
+						return small[mid - i - 1] < big[i + 1] ? small[mid - i - 1] : big[i + 1];
+					}
+					break;
+				}
 			}
 		}
 
 		if (flag) {
-			return big[left1];
+			return big[left1 + 1];
 		} else {
 			int left2 = (mid - 2 - left1);
-			int left = big[left1] > small[left2] ? big[left1] : small[left2];
-			int right = big[left1 + 1] > small[left2 + 1] ? small[left2 + 1] : big[left1 + 1];
+			int left;
+			if (left1 == -1) {
+				left = small[left2];
+			} else if (left2 == -1) {
+				left = big[left1];
+			} else {
+				left = big[left1] > small[left2] ? big[left1] : small[left2];
+			}
+			int right;
+			if (left2 + 1 >= small.length) {
+				right = big[left1 + 1];
+			} else if (left1 + 1 >= big.length) {
+				right = small[left2 + 1];
+
+			} else {
+
+				right = big[left1 + 1] > small[left2 + 1] ? small[left2 + 1] : big[left1 + 1];
+			}
 			return (double) (left + right) / 2;
 		}
 
 
 	}
 
-	public int binarySearch(int min, int max, int[] arr, int mid) {
-		int midIndex = (max + min) / 2;
-		if (arr[midIndex] > mid) {
-			if (midIndex == max) {
-				return max;
-			}
-			return binarySearch(min, midIndex, arr, mid);
-		} else if (arr[midIndex] < mid) {
-			if (midIndex == min) {
-				return min;
-			}
-			return binarySearch(midIndex, max, arr, mid);
-		} else {
-			return midIndex;
-		}
-	}
-
-
 	@Test
 	public void t1() {
-		int[] arr = {1,2};
-		int[] arr2 = {3,4};
+		int[] arr = {4};
+		int[] arr2 = {1, 2, 3};
 		double medianSortedArrays = findMedianSortedArrays(arr, arr2);
 		System.out.println(medianSortedArrays);
 		//int medianSortedArrays = binarySearch(0, arr2.length, arr2, 13);

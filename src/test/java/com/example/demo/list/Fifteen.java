104e5765bd5325aa93142b4171d1ba1f906a299f
caihuoche <yhy8191@163.com>
2019-09-06 11:36:43 +0800

    update:15

--- src/test/java/com/example/demo/list/Fifteen.java
+++ src/test/java/com/example/demo/list/Fifteen.java
@@ -1,11 +1,13 @@
 package com.example.demo.list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
 import org.junit.Test;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 
 /**
  * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
@@ -21,37 +23,76 @@ import org.junit.Test;
  **/
 public class Fifteen {
 
+	/**
+	 * 去重有点繁琐,没有继续下去
+	 */
 	public List<List<Integer>> threeSum(int[] nums) {
 		HashMap<Integer, Integer> set = new HashMap<>();
-		HashMap<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < nums.length; i++) {
 			set.put(nums[i], i);
 		}
 		List<List<Integer>> arrayList = new ArrayList<>();
 		for (int i = 0; i < nums.length; i++) {
-			//map.put(nums[i],i);
 			for (int j = i + 1; j < nums.length; j++) {
-			//	map.put(nums[j],j);
 				int temp = 0 - (nums[i] + nums[j]);
 				Integer integer = set.get(temp);
-				if (integer!=null && set.get(temp)>j  ) {
-						List<Integer> objects = new ArrayList<>();
-						objects.add(nums[i]);
-						objects.add(nums[j]);
-						objects.add(temp);
-						arrayList.add(objects);
-					//map.put(temp,integer);
+				if (integer != null && set.get(temp) > j) {
+					List<Integer> objects = new ArrayList<>();
+					objects.add(nums[i]);
+					objects.add(nums[j]);
+					objects.add(temp);
+					arrayList.add(objects);
 				}
 			}
-			map.clear();
+		}
+		return arrayList;
+	}
+
+	/**
+	 * 看了思路 才知道用一个指针固定,双指镇移动
+	 */
+	public List<List<Integer>> threeSum2(int[] nums) {
+		Arrays.sort(nums);
+		List<List<Integer>> arrayList = new ArrayList<>();
+		int length = nums.length;
+		for (int i = 0; i < nums.length - 2; i++) {
+			int r = length - 1;
+			if (i > 0) {
+				if (nums[i] == nums[i - 1]) {
+					continue;
+				}
+			}
+			for (int j = i + 1; j < r; j++) {
+				while (nums[i] + nums[j] + nums[r] < 0 && nums[j] < 0 && j < r - 1) {
+					j++;
+				}
+				while (nums[i] + nums[j] + nums[r] > 0 && nums[r] > 0 && j < r - 1) {
+					r--;
+				}
+				if (nums[i] + nums[j] + nums[r] == 0 ) {
+					if (j - 1 != i ){
+						if (nums[j] == nums[j - 1]){
+							continue;
+						}
+					}
+					List<Integer> objects = new ArrayList<>();
+					objects.add(nums[i]);
+					objects.add(nums[j]);
+					objects.add(nums[r]);
+					arrayList.add(objects);
+				}
+			}
+
 		}
 		return arrayList;
 	}
 
 	@Test
 	public void t1() {
-		int[] nums = {-1, 0, 1, 2, -1, -4};
-		List<List<Integer>> lists = threeSum(nums);
+		//	int[] nums = {-10,-3,-8,-3,4,-1,-2,-4,-8,-5};
+		int[] nums = {-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6};
+//		int[] nums = {-1, 0, 1, 2, -1, -4};
+		List<List<Integer>> lists = threeSum2(nums);
 		System.out.println(lists);
 	}
 }

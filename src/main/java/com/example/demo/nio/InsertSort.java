6b7757b4c627de3f08a64d2aad331b64569f8c90
caihuoche <yhy8191@163.com>
2019-09-30 22:46:05 +0800

    插入排序

--- src/main/java/com/example/demo/nio/InsertSort.java
+++ src/main/java/com/example/demo/nio/InsertSort.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 /**
  * 插入排序  拿出一个和前面的去比较 合适就插进去
+ *
  * @author yanghaiyu
  * @date 2019/09/30
  **/
@@ -12,8 +13,14 @@ public class InsertSort {
 	public static void main(String[] args) {
 		int[] arr = {2, 4325, 4, 7, 23, 6, 124, 767, 242, 124, 124, 34, 645, 12, 11, 45, 5, 6};
 
-		for (int i =0;i<arr.length;i++){
-
+		for (int i = 1; i < arr.length; i++) {
+			for (int j = i - 1; j >= 0; j--) {
+				if (arr[i] < arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
 		}
 
 		System.out.println(Arrays.toString(arr));

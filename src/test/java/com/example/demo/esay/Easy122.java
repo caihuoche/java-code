aa47fccfdead9e5dc129b31fe3fd1dc0044af0af
caihuoche <570805251@qq.com>
2020-06-23 16:42:12 +0800

    88 :任何时候都要有逆向思维. 反着推

---
+++ src/test/java/com/example/demo/esay/Easy122.java
@@ -0,0 +1,9 @@
+package com.example.demo.esay;
+
+/**
+ *
+ * @author yanghaiyu
+ * @date 2020/06/23 
+ **/
+public class Easy122 {
+}
--- src/test/java/com/example/demo/esay/Easy88.java
+++ src/test/java/com/example/demo/esay/Easy88.java
@@ -1,7 +1,6 @@
 package com.example.demo.esay;
 
 
-
 import org.junit.Test;
 
 /**
@@ -27,12 +26,61 @@ import org.junit.Test;
  * @date 2020/06/19 
  **/
 public class Easy88 {
+	/**
+	 * 第一种, 空间复杂度 O(n) 时间复杂度 O(2n), 速度很快,但是显然不符合题解.
+	 * @param nums1
+	 * @param m
+	 * @param nums2
+	 * @param n
+	 */
 	public void merge(int[] nums1, int m, int[] nums2, int n) {
-
+		int[] arr = new int[m + n];
+		m--;
+		n--;
+		for (int i = m + n + 1; i >= 0; i--) {
+			if (m >= 0 && n >= 0) {
+				arr[i] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+			} else {
+				arr[i] = m >= 0 ? nums1[m--] : nums2[n--];
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			nums1[i] = arr[i];
+		}
+		System.out.printf("arr");
 	}
+
 	@Test
-	public void t1(){
+	public void t1() {
+		int[] nums1 = {0};
+		int m = 0;
+		int[] nums2 = {1};
+		int n = 1;
+
+		merge2(nums1, m, nums2, n);
+
 	}
 
+	/**
+	 * 任何时候都要有逆向思维. 反着推
+	 * @param nums1
+	 * @param m
+	 * @param nums2
+	 * @param n
+	 */
+	public void merge2(int[] nums1, int m, int[] nums2, int n) {
+		m--;
+		n--;
+		for (int i = m + n + 1; i >= 0; i--) {
+			if (m >= 0 && n >= 0){
+				nums1[i] = nums1[m] < nums2[n] ? nums2[n--] : nums1[m--];
+
+			}else {
+				nums1[i] = m >= 0 ? nums1[m--] : nums2[n--];
+
+			}
+		}
+		System.out.printf("m");
+	}
 
 }
